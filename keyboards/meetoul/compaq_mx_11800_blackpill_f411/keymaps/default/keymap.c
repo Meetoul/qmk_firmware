d5b8c89bf456c78c081b4303f61aff1f36b390a2
Ihor Ivanov <toulmee@gmail.com>
2024-01-15 12:06:53 +0200

    Add second layer and some media keys
    Make right control key to enable second layer,
    and map right shift and arrows to media keys
    on second layer.
    
    Signed-off-by: Ihor Ivanov <toulmee@gmail.com>

--- keyboards/meetoul/compaq_mx_11800_blackpill_f411/keymaps/default/keymap.c
+++ keyboards/meetoul/compaq_mx_11800_blackpill_f411/keymaps/default/keymap.c
@@ -20,10 +20,14 @@
 *                                                                                  '-----'-----'-----'
 */
 
+
+#define ____ KC_TRANSPARENT
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Custom layout:
     *   F1-F12 key swapped
     *   "Pause|Break" as media "Play/Pause"
+    *   Right control as FN
     * ,-----.     ,-----------------------------------.   ,-----------------------.   ,------.       ,-----------------------.
     * | Esc |     |  F7 |  F8 |  F9 | F10 | F11 | F12 |   | Ins | Home| PgUp| Prsc|   | Pl/Ps|       | Nlck|  /  |  *  |  -  |
     * '-----'     |-----------------------------------+   |-----------------------+   '------'       |-----------------------+
@@ -38,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |----------------------------------------------------------------------------------------'
     * |   Shift    |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |    Shift  | ,-----.
     * |------------------------------------------------------------------------------------' |  ^  |
-    * |  Ctrl  |   Alt  |                  Space                  |   Alt  |  Ctrl  |  ,-----.-----.-----.
+    * |  Ctrl  |   Alt  |                  Space                  |   Alt  |  FN    |  ,-----.-----.-----.
     * '-----------------------------------------------------------------------------'  |  <  |  v  |  >  |
     *                                                                                  '-----'-----'-----'
     */
@@ -51,6 +55,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,               KC_P0,            KC_PDOT,
         KC_CAPS,       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,            KC_UP,
-        KC_LCTL,     KC_LALT,                             KC_SPC,                                 KC_LWIN,     KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
-    )
+        KC_LCTL,     KC_LALT,                             KC_SPC,                                 KC_LWIN,       MO(1),          KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+    [1] = LAYOUT_def(
+        ____,             ____,    ____,    ____,    ____,    ____,    ____,        ____,    ____,    ____,    ____,         ____,                      ____, ____, ____, ____,
+                          ____,    ____,    ____,    ____,    ____,    ____,        ____,    ____,    ____,    ____,                                    ____, ____, ____, ____,
+                                                                                                                                                        ____, ____, ____,
+        ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,                      ____, ____, ____, ____,
+        ____,        ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,                  ____,       ____,
+        ____,          ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,
+        ____,               ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    ____,    KC_MUTE,            KC_VOLU,
+        ____,        ____,                                ____,                                    ____,         MO(1),          KC_MPRV, KC_VOLD, KC_MNXT
+    ),
 };
